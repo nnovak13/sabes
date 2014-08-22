01967f45928d8947387a1f004069b4edabd36987
brimatteng <btenggren20@gmail.com>
2014-08-22 16:54:13 -0400

    add routes for homepage!

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 var sabesApp = angular.module('sabesApp', ['ngRoute']);
+
+//Routing
+sabesApp.config(['$routeProvider', function($routeProvider){
+  // path -> view -> controller
+  $routeProvider
+    .when('/home',
+    {
+      templateUrl: 'app/views/home.html'
+    })
+    .otherwise({redirectTo: '/home'});
+}]);
