a05c07ef4bd668d1fde5c026a799767b647c5801
nnovak13 <nnovak@fordham.edu>
2014-08-23 20:44:08 -0400

    fixed some bullshit goin on in them there seed file and added artist_id to articles.

--- db/migrate/20140823220946_create_articles.rb
+++ db/migrate/20140823220946_create_articles.rb
@@ -1,7 +1,6 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-
       t.string :article_title
       t.string :article_body
       t.string :article_img
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823220946) do
+ActiveRecord::Schema.define(version: 20140824001141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20140823220946) do
     t.string   "article_img"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
+
+  add_index "articles", ["artist_id"], name: "index_articles_on_artist_id", using: :btree
 
   create_table "artists", force: true do |t|
     t.integer  "user_id",            null: false
